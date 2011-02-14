4b39d74942ef693db6b7a95536cb8756fc3cbbc3
roger.rudin <roger.rudin@1f050706-6a62-311d-6bb8-b63aafb22fd9>
2011-02-14 21:43:55 +0000

    build script adjusted

--- trunk/scripts/integrate.bat
+++ trunk/scripts/integrate.bat
@@ -1,7 +1,10 @@
 @echo off
 if exist "%ProgramFiles%\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" call "%ProgramFiles%\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
+set resultDir=..\results
+if not exist %resultDir% mkdir %resultDir%
 
-..\Tools\nant-0.91-alpha2\bin\NAnt -buildfile:nant.build integrate 
+
+..\Tools\NAnt\NAnt -buildfile:nant.build -l:%resultDir%\FinancePlus-Nant-Build.log integrate 
 IF ERRORLEVEL 1 GOTO Failed
 
 echo "compilation and unit testing completed. Log file and unit-tests results are stored in %resultDir%"
